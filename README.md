909a6b190706d5a98140e6a013714cda0f0e068f
zlaty12 <zlatimirbg2@gmail.com>
2024-02-03 10:12:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TowerDefenceAI
+
+Developed with Unreal Engine 5
---
+++ Source/TowerDefenceAI/TowerDefenceAI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TowerDefenceAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TowerDefenceAI, "TowerDefenceAI" );
